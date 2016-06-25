1442be69627618865e96206d23279f238df641a7
Nemikolh <joan.varvenne@gmail.com>
2016-06-25 12:20:29 +0100

    Fix issues in dev env.

--- spells/webpack.config.js/dev.js
+++ spells/webpack.config.js/dev.js
@@ -3,10 +3,10 @@ const path = require('path');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./common');
 const DefinePlugin = require('webpack').DefinePlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const outputDir = path.join(path.dirname(__dirname), '/dev/');
 
-writeHtmlEntryPoint();
 
 module.exports = webpackMerge(commonConfig, {
     debug: true,
@@ -30,28 +30,11 @@ module.exports = webpackMerge(commonConfig, {
             VERSION: "1.0",
             IS_PRODUCTION: false,
         }),
+        new HtmlWebpackPlugin({
+          template: 'src/index.html',
+          chunksSortMode: 'dependency',
+          inject: false,
+          is_dev: true,
+        }),
     ],
 });
-
-function writeHtmlEntryPoint() {
-  const fs = require('fs');
-  const content = `
-  <!doctype html>
-  <html>
-  <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-      <title>Spells (dev mode)</title>
-      <script src="polyfills.js"></script>
-      <script src="vendor.js"></script>
-      <script src="libs-free.js"></script>
-  </head>
-  <body>
-      <app></app>
-      <script src="index.js"></script>
-  </body>
-  </html>
-  `;
-  fs.writeFileSync(outputDir + 'index.html', content);
-}
--- spells/webpack.config.js/index.js
+++ spells/webpack.config.js/index.js
@@ -1,22 +1,44 @@
 const docopt = require('docopt').docopt;
 const colors = require('colors/safe');
+const noop = require('./util').colors_noop;
+const resolve_env = require('./util').resolve_env;
+const current_env = require('./util').current_env;
 
-let USAGE = `
-Webpack options
 
-Usage:
+let USAGE = (colors) => `
+${colors.bold.green('Webpack custom options')}
+
+Those options allows to override the default values fetch using
+the environment variable 'NODE_ENV'.
+
+Your current value is:
+
+    '${current_env()}'
+
+Thus the environment will be:
+
+    '${resolve_env()}'
+
+The following options allows you to override this behavior.
+
+${colors.bold.green('Usage:')}
   webpack [--prod | --dev]
-  webpack -h | --help | --version
+  webpack --opt-help | --version
 
-Arguments:
-  -h --help             Show this message.
+${colors.bold.green('Arguments:')}
+  --opt-help            Show this message.
   --version             Show the version of this tool.
   --prod                Force a production compilation.
   --dev                 Force a dev environment.
 `;
 
-let options = docopt(USAGE, {version: "1.0.0"});
+let options = docopt(USAGE(noop), {version: "1.0.0"});
 let mode = process.env.NODE_ENV;
+
+if (options['--opt-help']) {
+    console.log(USAGE(colors));
+    process.exit();
+}
 
 if (options['--prod']) {
   console.log(colors.bold("Forcing production mode"));
