a6393563c1dea7c2fe2bc8b8fb8e9cb3e0f45620
Nemikolh <joan.varvenne@gmail.com>
2016-06-30 18:19:23 +0100

    Fix build

--- spells/src/index.ts
+++ spells/src/index.ts
@@ -11,10 +11,12 @@ import {App} from './app';
 import {GameEngine} from './game/engine';
 import {SpellEngine} from './spells/engine';
 import {Scene} from './models/scene';
+import {SpellList} from './models/spell_list';
 
 bootstrap(App, [
   provide(LocationStrategy, { useClass: HashLocationStrategy }),
   Scene,
   GameEngine,
-  SpellEngine
+  SpellEngine,
+  SpellList
 ]);
